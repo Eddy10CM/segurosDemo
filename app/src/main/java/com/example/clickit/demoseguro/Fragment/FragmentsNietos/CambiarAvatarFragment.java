66dd86c6d3075e7c45ba1e598105b9bd8e6df3fe
fcolfigueroa@outlook.com <FranciscoLFigueroa>
2016-09-01 14:14:15 -0500

    camara sin funcionar

--- app/src/main/java/com/example/clickit/demoseguro/Fragment/FragmentsNietos/CambiarAvatarFragment.java
+++ app/src/main/java/com/example/clickit/demoseguro/Fragment/FragmentsNietos/CambiarAvatarFragment.java
@@ -1,18 +1,29 @@
 package com.example.clickit.demoseguro.Fragment.FragmentsNietos;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.frosquivel.magicalcamera.MagicalCamera;
-
 import com.example.clickit.demoseguro.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,15 +34,23 @@ public class CambiarAvatarFragment extends Fragment {
 
     private Activity activity;
     private CircleImageView imagecircle;
-    private MagicalCamera magicalCamera;
-    private int RESIZE_PHOTO_PIXELS_PERCENTAGE = 3000;
+    private static final int REQUEST_CAPTURE = 1;
+    private File imageFile;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         activity = getActivity();
 
-        magicalCamera = new MagicalCamera(activity,RESIZE_PHOTO_PIXELS_PERCENTAGE);
+        PackageManager packageManager = activity.getPackageManager();
+
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA) == false){
+            Toast.makeText(getActivity(),"This device doesn't have a camera",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getActivity(),"This device have a camera",Toast.LENGTH_SHORT).show();
+        }
 
         View view = inflater.inflate(R.layout.fragment_cambiar_avatar,container,false);
 
@@ -40,28 +59,31 @@ public class CambiarAvatarFragment extends Fragment {
         imagecircle.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                /*if (magicalCamera.takeFragmentPhoto()) {
-                    startActivityForResult(magicalCamera.getIntentFragment(),MagicalCamera.TAKE_PHOTO);
-                }*/
-                magicalCamera.takePhoto();
+                launchCamera(v);
             }
+
         });
 
         return view;
     }
 
-    /*@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        magicalCamera.resultPhoto(requestCode, resultCode, data);
-        imagecircle.setImageBitmap(magicalCamera.getMyPhoto());
-        //magicalCamera.savePhotoInMemoryDevice(magicalCamera.getMyPhoto(), "myTestPhoto", MagicalCamera.JPEG, true);
+    public void launchCamera(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imageFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"Test.jpg");
+        Uri tempUri = Uri.fromFile(imageFile)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,value);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY);
+        startActivityForResult(intent, REQUEST_CAPTURE);
+    }
 
-        if(magicalCamera.savePhotoInMemoryDevice(magicalCamera.getMyPhoto(), "myTestPhoto", MagicalCamera.JPEG, true)){
-            Toast.makeText(activity, "The photo is save", Toast.LENGTH_SHORT);
-        }
-        else {
-            Toast.makeText(activity, "The photo isn't save",Toast.LENGTH_SHORT);
+
+    /*@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CAPTURE && resultCode == activity.RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            imagecircle.setImageBitmap(photo);
         }
     }*/
 }
